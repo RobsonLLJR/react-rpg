5d80c7b8f55e492dfd29e1e14bc6a78385dffcdd
Robson Liesner de Lima Junior <robsonlljr@gmail.com>
2024-01-06 12:05:34 -0300

    Feito tabela para listagem dos Characters provinda do endpoint 'api/Character/GetAll' do projeto dotnet.rpg

--- src/CRUD.js
+++ src/CRUD.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect, Fragment } from "react";
 import Table from "react-bootstrap/Table";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Col, Container, Row } from "react-bootstrap";
+import axios from "axios";
+import Form from 'react-bootstrap/Form';
 
 const CRUD = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const empdata = [
     {
       id: 1,
@@ -23,7 +33,7 @@ const CRUD = () => {
       class: 1,
     },
     {
-      id: 2,
+      id: 3,
       name: "Eduarda",
       hitPoints: 29,
       strength: 1,
@@ -35,12 +45,112 @@ const CRUD = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    setData(empdata);
+    getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const getData = () => {
+    axios
+      .get("https://localhost:44322/api/Character/GetAll")
+      .then((result) => {
+        setData(result.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleEdit = (id) => {
+    //alert(id);
+    handleShow();
+  };
+  const handleDelete = (id) => {
+    if (window.confirm("Do you really want to delete this item?")) {
+      alert(id);
+    }
+  };
+
+  const [name, setName] = useState("");
+  const [hitPoints, setHitPoints] = useState("");
+  const [strenght, setStrenght] = useState("");
+  const [defense, setDefense] = useState("");
+  const [intelligence, setIntelligence] = useState("");
+  const [classe, setClasse] = useState("");
+
+  const [editId, setEditId] = useState("");
+  const [editName, setEditName] = useState("");
+  const [editHitPoints, setEditHitPoints] = useState("");
+  const [editStrenght, setEditStrenght] = useState("");
+  const [editDefense, setEditDefense] = useState("");
+  const [editIntelligence, setEditIntelligence] = useState("");
+  const [editClasse, setEditClasse] = useState("");
+  const handleUpdate = () => {};
+
   return (
     <Fragment>
+      <Container className="m-2">
+        <Row>
+          <Col>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </Col>
+          <Col>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="HitPoints"
+              value={hitPoints}
+              onChange={(e) => setHitPoints(e.target.value)}
+            ></input>
+          </Col>
+          <Col>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Strength"
+              value={strenght}
+              onChange={(e) => setStrenght(e.target.value)}
+            ></input>
+          </Col>
+          <Col>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Defense"
+              value={defense}
+              onChange={(e) => setDefense(e.target.value)}
+            ></input>
+          </Col>
+          <Col>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Intelligence"
+              value={intelligence}
+              onChange={(e) => setIntelligence(e.target.value)}
+            ></input>
+          </Col>
+          <Col>
+            <Form.Select
+              value={classe}
+              onChange={(e) => setClasse(e.target.value)}
+            >
+              <option>Open this select menu</option>
+              <option value="1">Knight</option>
+              <option value="2">Mage</option>
+              <option value="3">Cleric</option>
+            </Form.Select>
+          </Col>
+          <Col>
+            <button className="btn btn-primary">Submit</button>
+          </Col>
+        </Row>
+      </Container>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -52,26 +162,120 @@ const CRUD = () => {
             <th>Defense</th>
             <th>Intelligence</th>
             <th>Class</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          
-          {data && data.length > 0 ? data.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td>{index+ 1}</td>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.hitPoints}</td>
-                <td>{item.strength}</td>
-                <td>{item.defense}</td>
-                <td>{item.intelligence}</td>
-                <td>{item.class}</td>
-              </tr>
-            );
-          }) : 'Carregando...'}
+          {data && data.length > 0
+            ? data.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.hitPoints}</td>
+                    <td>{item.strength}</td>
+                    <td>{item.defense}</td>
+                    <td>{item.intelligence}</td>
+                    <td>{item.class}</td>
+                    <td colSpan={2}>
+                      <button
+                        className="btn btn-primary me-2"
+                        onClick={() => handleEdit(item.id)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => handleDelete(item.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })
+            : "Carregando..."}
         </tbody>
       </Table>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modify Character</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container className="m-2">
+            <Row>
+              <Col>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  value={editName}
+                  onChange={(e) => setEditName(e.target.value)}
+                ></input>
+              </Col>
+              <Col>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="HitPoints"
+                  value={editHitPoints}
+                  onChange={(e) => setEditHitPoints(e.target.value)}
+                ></input>
+              </Col>
+              <Col>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Strength"
+                  value={editStrenght}
+                  onChange={(e) => setEditStrenght(e.target.value)}
+                ></input>
+              </Col>
+              <Col>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Defense"
+                  value={editDefense}
+                  onChange={(e) => setEditDefense(e.target.value)}
+                ></input>
+              </Col>
+              <Col>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Intelligence"
+                  value={editIntelligence}
+                  onChange={(e) => setEditIntelligence(e.target.value)}
+                ></input>
+              </Col>
+              <Col>
+                <Form.Select
+                  value={editClasse}
+                  onChange={(e) => setEditClasse(e.target.value)}
+                >
+                  <option>Open this select menu</option>
+                  <option value="1">Knight</option>
+                  <option value="2">Mage</option>
+                  <option value="3">Cleric</option>
+                </Form.Select>
+              </Col>
+              <Col>
+                <button className="btn btn-primary">Submit</button>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleUpdate}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Fragment>
   );
 };
